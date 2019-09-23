6de7dc5597b86c603a2c519e221b3fec523740c9
Le Khac Man <andrew_le@aviva-asia.com>
2019-09-23 15:08:55 +0800

    Refactor primitive gate to Nand

--- src/1/gates.go
+++ src/1/gates.go
@@ -1,22 +1,22 @@
 package gates
 
-// --- Primitive Gates ---
+// --- Given Primitive Gates ---
+func Nand(x, y bool) bool {
+	return !(x && y)
+}
+
+// --- End of Given Primitive Gates ---
+
 func Not(x bool) bool {
-	return !x
+	return Nand(x, x)
 }
 
 func And(x, y bool) bool {
-	return x && y
+	return Not(Nand(x, y))
 }
 
 func Or(x, y bool) bool {
-	return x || y
-}
-
-// --- End of Primitive Gates ---
-
-func Nand(x, y bool) bool {
-	return Not(And(x, y))
+	return Nand(Not(x), Not(y))
 }
 
 func Xor(x, y bool) bool {
@@ -25,13 +25,15 @@ func Xor(x, y bool) bool {
 
 // If sel = true, return y, else return x
 func Mux(x, y, sel bool) bool {
-	return (x && !y && !sel) || (x && y && !sel) || (!x && y && sel) || (x && y && sel)
-	//return Or(
-	//  Or(
-	//
-	//  ),
-	//  Or(
-	//
-	//  ),
-	//)
+	//return (x && !y && !sel) || (x && y && !sel) || (!x && y && sel) || (x && y && sel)
+	return Or(
+		Or(
+			And(x, And(Not(y), Not(sel))),
+			And(And(x, y), Not(sel)),
+		),
+		Or(
+			And(Not(x), And(y, sel)),
+			And(And(x, y), sel),
+		),
+	)
 }
