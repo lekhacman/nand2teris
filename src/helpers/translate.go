00b308172bca2017e5fb25163e841aff34643b4f
andrew <khacmanle@gmail.com>
2019-09-25 23:19:29 +0800

    translate binary string to boolean string

--- src/helpers/translate.go
+++ src/helpers/translate.go
@@ -1,17 +1,49 @@
 package helpers
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
-func ToBoolean(str string) [16]bool {
-	mapper := func(fn func(d string) bool, functor []string) [16]bool {
-		var result [16]bool
-		for i := 0; i < len(functor); i++ {
-			result[i] = fn(functor[i])
-		}
-		return result
+func reduce(
+	fn func(acc string, cur string) string,
+	init string,
+	f []string,
+) string {
+	out := init
+	for _, item := range f {
+		out = fn(out, item)
 	}
-	isOne := func(d string) bool { return "1" == d }
-	digit := strings.Split(str, "")
+	return out
+}
 
-	return mapper(isOne, digit)
+func mapp(fn func(string) string, f []string) []string {
+	out := make([]string, len(f))
+	for id, item := range f {
+		out[id] = fn(item)
+	}
+	return out
+}
+
+func ToBoolean(str string) string {
+	binaryToBool := func(b string) string {
+		if b == "0" {
+			return "false"
+		}
+		return "true"
+	}
+	connect := func(acc, item string) string {
+		return fmt.Sprintf("%s, %s", acc, item)
+	}
+	bools := mapp(
+		binaryToBool,
+		strings.Split(str, ""),
+	)
+	var out = reduce(
+		connect,
+		bools[0],
+		bools[1:],
+	)
+
+	return fmt.Sprintf("{%s}", out)
 }
--- src/helpers/translate_test.go
+++ src/helpers/translate_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestToBoolean(t *testing.T) {
 	input := "0011110011000011"
-	want := [16]bool{false, false, true, true, true, true, false, false, true, true, false, false, false, false, true, true}
+	want := "{false, false, true, true, true, true, false, false, true, true, false, false, false, false, true, true}"
 
 	got := ToBoolean(input)
 
