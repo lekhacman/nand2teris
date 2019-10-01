586a27a87838c1e75629dfb948018e703bbd2375
Le Khac Man <andrew_le@aviva-asia.com>
2019-10-01 17:36:06 +0800

    setup project 02

--- src/gates/arithmeticChip_test.go
+++ src/gates/arithmeticChip_test.go
@@ -1,6 +1,32 @@
 package gates
 
-import "testing"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"testing"
+)
 
 func TestHalfAdder(t *testing.T) {
+	path, _ := filepath.Abs("./chips/02/HalfAdder.cmp.json")
+	testPlanPath, _ := ioutil.ReadFile(path)
+	var cases []struct {
+		A     bool
+		B     bool
+		Sum   bool
+		Carry bool
+	}
+	if err := json.Unmarshal(testPlanPath, &cases); err != nil {
+		log.Fatal(err)
+	}
+
+	for id, c := range cases {
+		sum, carry := HalfAdder(c.A, c.B)
+		got := [2]bool{sum, carry}
+		want := [2]bool{c.Sum, c.Carry}
+		if got != want {
+			t.Errorf("#%d HalfAdder\ngot:%v\nwan:%v\n", id+1, got, want)
+		}
+	}
 }
