c0df575580184ae774cfed4baddf8afbe87858f9
andrew <khacmanle@gmail.com>
2019-10-01 21:40:12 +0800

    [Chip][FullAdder] implementation

--- src/gates/arithmeticChip.go
+++ src/gates/arithmeticChip.go
@@ -5,3 +5,10 @@ func HalfAdder(a, b bool) (sum, carry bool) {
 	sum = Xor(a, b)
 	return
 }
+
+func FullAdder(a, b, c bool) (sum, carry bool) {
+	xorBC := Xor(b, c)
+	sum = Mux(xorBC, Not(xorBC), a)
+	carry = Mux(And(b, c), Or(b, c), a)
+	return
+}
