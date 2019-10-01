b6ebd326d0679d816f1357b2b45e0c731e884ae5
andrew <khacmanle@gmail.com>
2019-10-01 23:09:55 +0800

    [Chip][Add16] Add 16 bit binary number

--- src/gates/arithmeticChip.go
+++ src/gates/arithmeticChip.go
@@ -1,5 +1,7 @@
 package gates
 
+import "fmt"
+
 func HalfAdder(a, b bool) (sum, carry bool) {
 	carry = And(a, b)
 	sum = Xor(a, b)
@@ -16,6 +18,27 @@ func FullAdder(a, b, c bool) (sum, carry bool) {
 	return
 }
 
+//func add2(a, b [2]bool) (out [2]bool) {
+//  return
+//}
+//
+//func add4(a, b [4]bool) (out [4]bool) {
+//  return
+//}
+//
+//func add8(a, b [8]bool) (out [8]bool) {
+//  return
+//}
+
 func Add16(a, b [16]bool) (out [16]bool) {
+	carry := false
+	id := len(out) - 1
+	for id >= 0 {
+		out[id], carry = FullAdder(a[id], b[id], carry)
+		id--
+	}
+	if carry {
+		fmt.Println("Overflow!")
+	}
 	return
 }
