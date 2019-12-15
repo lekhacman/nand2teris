34a76e6c523ed3d043127069116eb00cb24fad7e
Le Khac Man <khacmanle@gmail.com>
2019-12-15 15:10:29 +0800

    [SCREEN] Fill screen on keydown action

--- src/04/fill/Fill.asm
+++ src/04/fill/Fill.asm
@@ -1,9 +1,52 @@
 // keyboard = RAM[24576] screen = RAM[16384] - RAM[]
   @i
   M=0
-(KEYDOWN)
+  @16384
+  D=A
+  @scr
+  M=D
+  @8191
+  D=A
+  @limit
+  M=D
+(REFRESH)
+  @KBD
+  D=M
+  @HIDE
+  D;JEQ
 
+  @SHOW
+  0;JMP
 
 (SHOW)
+  @i
+  D=M
+  @scr
+  A=D+M
+  M=-1
 
+  @limit
+  D=D-M
+  @REFRESH
+  D;JEQ
+
+  @i
+  M=M+1
+  @REFRESH
+  0;JMP
 (HIDE)
+  @i
+  D=M
+
+  @scr
+  A=M+D
+  M=0
+
+  @REFRESH
+  D;JEQ
+
+  @i
+  M=M-1
+
+  @REFRESH
+  0;JMP
