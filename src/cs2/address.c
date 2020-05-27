b817aabca90d4dcc4c980de517990647be14420c
Andrew Le <andrewlkm@spgroup.com.sg>
2020-05-27 22:19:17 +0800

    My mac is using little endian and 64 bit word size

---
+++ src/cs2/address.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0x01234567; // 19088743
+
+    printf("%p\n", &i);
+    printf("%d\n", i);
+
+    return 0;
+}
--- src/cs2/endian.c
+++ src/cs2/endian.c
@@ -2,17 +2,41 @@
 
 typedef unsigned char *bytePointer;
 
-void showBytes(bytePointer data, size_t len);
+void showInt(int x);
+void showFloat(float x);
+void showPointer(void *x);
 
 int main() {
     int num = 12345;
-    showBytes((bytePointer) &num, sizeof(int));
+    showInt(num);
+
+    int i = 0x01234567; // 19088743
+    showInt(i); // 67 45 23 01 => little endian
+    printf("pointer of i: %p\n", &i);
+    showPointer(&i);
+
+    float pi = 3.14;
+    showFloat(pi);
+
 }
 
 void showBytes(bytePointer data, size_t len) {
+    printf("Size: %d bytes\n", len);
+
     for (int i = 0; i < len; ++i) {
         printf("%.2x ", data[i]);
     }
     printf("\n");
 }
 
+void showInt(int x) {
+    showBytes((bytePointer) &x, sizeof(int));
+}
+
+void showFloat(float x) {
+    showBytes((bytePointer) &x, sizeof(float));
+}
+
+void showPointer(void *x) {
+    showBytes((bytePointer) &x, sizeof(void *));
+}
