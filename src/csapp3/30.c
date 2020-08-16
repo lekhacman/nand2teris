f3693f57fb6151c30b55286da614a915b8b03687
andrew <khacmanle@gmail.com>
2020-08-16 20:27:30 +0800

    chap 3: ex30 switch assembly

---
+++ src/csapp3/30.c
@@ -0,0 +1,23 @@
+void switch2(short x, short *dest) {
+    short val = 0;
+
+    // x > 6 jmp default
+
+    switch (x) {
+        case -2: // L9
+            break;
+        case 0: // L6
+            break;
+        case 1: // L7
+        case 3: // L7
+            break;
+        case 4: // L8
+        break;
+        case -1: // L5
+        case 6: // L5
+        default:
+
+    }
+
+    *dest = val;
+}
